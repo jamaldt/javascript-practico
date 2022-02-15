9d8d35230b037384d02538a21306bc21938e9b86
jamal <carldeltoro@gmail.com>
2022-02-14 21:36:06 -0400

    redux example in javascript

---
+++ redux-contador/index.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux'
+
+const contador = document.getElementById('contador')
+const decrementar = document.getElementById('decrementar')
+const incrementar = document.getElementById('incrementar')
