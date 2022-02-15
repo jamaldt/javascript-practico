e0aae0a2ed181339a4c92c7c6f828922c7111424
jamal <carldeltoro@gmail.com>
2022-02-14 21:42:05 -0400

    contador redux

--- redux-contador/index.js
+++ redux-contador/index.js
@@ -3,3 +3,22 @@ import {createStore} from 'redux'
 const contador = document.getElementById('contador')
 const decrementar = document.getElementById('decrementar')
 const incrementar = document.getElementById('incrementar')
+
+const INITIAL_STATE = {
+    counter:0
+}
+
+function counterApp(state = INITIAL_STATE,action){
+    console.log(state,action)
+    return state
+}
+
+const store = createStore(counterApp)
+
+incrementar.addEventListener('click',()=> {
+    store.dispatch({type:'INCREMENT'})
+})
+
+decrementar.addEventListener('click',()=> {
+    store.dispatch({type:'DECREMENT'})
+})
